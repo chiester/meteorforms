6217c62de3cf9ccb534c460335c921a73c9e686f
Christopher Hiester <chris.hiester@gmail.com>
2015-11-18 00:11:58 -0500

    added toggle

--- client/form/form.js
+++ client/form/form.js
@@ -8,16 +8,20 @@ Template.formProposal.rendered = function() {
 
 Template.formProposal.events({
   'click .js-switch': function(event, template) {
+    var lt, mt;
 
-    console.log(event);
-
-    //kind of ugly
-    if (event.target.name === 'employer.numberOfEmployees.lessThanTen') {
-      console.log($("input[name='employer.numberOfEmployees.moreThanTen']").prop("checked"));
-      $("input[name='employer.numberOfEmployees.moreThanTen']")[0].prop("checked", false);
-    } else if (event.target.name === 'employer.numberOfEmployees.moreThanTen') {
-      $("input[name='employer.numberOfEmployees.lessThanTen']")[0].prop("checked", false);
+    // poor man's toggle
+    if (event.target.name === 'employer.numberOfEmployees.lessThanTen' && event.target.checked) {
+      //console.log($("input[name='employer.numberOfEmployees.moreThanTen']"));
+      mt = $("input[name='employer.numberOfEmployees.moreThanTen']")[0];
+      if (mt.checked) {
+        $('.switchery').click();
+      }
+    } else if (event.target.name === 'employer.numberOfEmployees.moreThanTen' && event.target.checked) {
+      lt = $("input[name='employer.numberOfEmployees.lessThanTen']")[0];
+      if (lt.checked) {
+        $('.switchery').click();
+      }
     }
-
   }
-})
+});
